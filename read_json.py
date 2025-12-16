4e79166342b00b2603456209fdb67979d9c46250
llada60 <572192783@qq.com>
2025-12-16 11:34:31 +0100

    func: transfer json into prompt data

--- read_json.py
+++ read_json.py
@@ -1,20 +1,84 @@
-import json
 from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig, pipeline
+from langchain_core.output_parsers import PydanticOutputParser, JsonOutputParser
+from langchain_core.prompts import PromptTemplate
 
-def load_json(file_path):
+from pydantic import BaseModel
+from typing import Literal
+import json
+
+def json2dicList(file_path):
     with open(file_path, 'r') as f:
         data = json.load(f)
-    return data
+    flatten_data = []
+    idx = 0
+    for key, value in data.items():
+        statements = value[0]
+        labels = value[1]
+        table_title = value[2]
+        table = value[3]
+        for statement, label in zip(statements, labels):
+            flatten_data.append({
+                "id": idx,
+                "statement": statement,
+                "label": label,
+                "table_title": table_title,
+                "table": table
+            })
+            idx += 1
+    return flatten_data
 
-datas = load_json('./data/test_examples_with_csv.json')
-max_len = 0
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-7B-Instruct-1M")
-llm = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-7B-Instruct-1M")
-print(llm.config.max_position_embeddings)
-
-for key, value in datas.items():
-    table = value[3]
-    tokens = tokenizer(table)
-    max_len = max(max_len, len(tokens['input_ids']))
+def dicList2prompt(dic_list, prompt_template, parser):
+    promptDataset = []
+    for item in dic_list:
+        prompt = prompt_template.format(
+            statement=item['statement'],
+            table_title=item['table_title'],
+            table=item['table']
+        )
+        promptDataset.append(
+            {
+                "id": item['id'],
+                "prompt": prompt,
+                "label": item['label']
+            }
+        )
+    return promptDataset
     
-print("Max table length:", max_len)
+
+class ResponseSchema(BaseModel):
+    # reasoning: str
+    answer: Literal["Supported", "Refuted"]
+    
+    
+if __name__ == "__main__":
+    # transfer to flattened json
+    # flatten_data = json2dicList('./data/test_examples_with_csv.json') 
+    # with open('./data/flattened_test_examples_with_csv.json', 'w') as f:
+    #     json.dump(flatten_data, f)
+        
+    json_path = './data/flattened_test_examples_with_csv.json'
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+        
+    # save prompt label json
+    parser = JsonOutputParser(pydantic_object=ResponseSchema)
+        
+        # Analyze in one or two sentences and 
+    single_prompt_template = """You will be provided with a statement and a table. Determine whether the statement is supported or refuted by the information in the table.
+    {format_instructions}
+    
+    Statement: {statement}
+    Table Title: {table_title}
+    Table: {table}
+    """
+    single_prompt = PromptTemplate(
+        template = single_prompt_template,
+        input_variables = ["statement", "table_title", "table"],
+        partial_variables={"format_instructions": parser.get_format_instructions()}
+    )
+    
+    promptDataset = dicList2prompt(data, single_prompt, parser)
+    with open('./data/promptDataset/single_prompted_test_examples.json', 'w') as f:
+        json.dump(promptDataset, f)
+
+
