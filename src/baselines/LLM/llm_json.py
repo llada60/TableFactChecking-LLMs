93ea9fe3b4cbbcb3f82c13535a7a98c340bd4591
llada60 <572192783@qq.com>
2025-12-22 18:49:00 +0100

    fix_bug: serial

--- src/baselines/LLM/llm_json.py
+++ src/baselines/LLM/llm_json.py
@@ -9,6 +9,7 @@ from typing import Literal
 from tqdm import tqdm
 import json
 import argparse
+import re
 
 
 def load_json(file_path):
@@ -29,23 +30,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, default='./data/test_examples_with_csv_paraphrased.json', help='Path to the input JSON data file')
     parser.add_argument('--model_name', type=str, default='Qwen/Qwen2.5-7B-Instruct-1M', help='Name of the pre-trained model to use')
-    parser.add_argument('--max_new_tokens', type=int, default=10, help='Maximum number of new tokens to generate')
+    parser.add_argument('--max_new_tokens', type=int, default=90, help='Maximum number of new tokens to generate')
     parser.add_argument('--temperature', type=float, default=0.0, help='Temperature for text generation')
+    parser.add_argument('--do_sample', type=bool, default=False, help='Whether to use sampling for text generation')
     args = parser.parse_args()
     
     model_name = args.model_name
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    llm = AutoModelForCausalLM.from_pretrained(model_name)
-    hf_pipe = pipeline(
-        "text-generation",
-        model=llm,
-        tokenizer=tokenizer,
-        temperature=args.temperature,
-        max_new_tokens=args.max_new_tokens,
-        do_sample=False,
-        return_full_text=False,
-    )
-    hf_llm = HuggingFacePipeline(pipeline=hf_pipe)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    
+    llm = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
+    llm.eval()
     
     data = load_json(args.data_path)
     pbar = tqdm(data, total=len(data))
@@ -70,23 +65,32 @@ if __name__ == "__main__":
         input_variables = ["statement", "table_title", "table"],
         partial_variables={"format_instructions": parser.get_format_instructions()}
     )
-    chain = prompt | hf_llm
-    
     correct = 0
     wrong = 0
     total = 0
     for value in pbar:
+        total += 1
         pbar.set_description(f"acc:{correct}/{total}, wrong:{wrong}")
-        total += len(statements)
         label = value["label"]
-        result = chain.invoke({
-            "statement": value["statement"],
-            "table_title": value["table_title"],
-            "table": value["table"]
-        })
+        prompt_text = prompt.format(
+            statement=value["statement"],
+            table_title=value["table_title"],
+            table=value["table"]
+        )
+        tokens = tokenizer(prompt_text, return_tensors='pt').to(llm.device)
+        print(tokens['input_ids'].shape)
+        outputs = llm.generate(
+            **tokens,
+            max_new_tokens=args.max_new_tokens,
+            do_sample=args.do_sample,
+            temperature=args.temperature,   
+        )[0][tokens['input_ids'].shape[-1]:]
+        result = tokenizer.decode(outputs, skip_special_tokens=True)
+        print(result)
+        # print(result)
         json_str = extract_json(result)
         if json_str is None:
-            continue
+            json_str = result
         try:
             parsed_output = parser.parse(json_str)["answer"]
         except: 
