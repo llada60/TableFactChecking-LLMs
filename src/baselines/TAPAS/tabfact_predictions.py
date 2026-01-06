3c060b40e9485253e73933764b35077a97e9caa8
llada60 <572192783@qq.com>
2026-01-06 14:35:03 +0100

    change the structure of tapas py

--- src/baselines/TAPAS/tabfact_predictions.py
+++ src/baselines/TAPAS/tabfact_predictions.py
@@ -1,13 +1,6 @@
 import argparse
-
-
 from tensorflow.python.client import device_lib
 from tqdm import tqdm
-
-
-# In[3]:
-
-
 import tensorflow.compat.v1 as tf
 import os 
 import shutil
@@ -17,35 +10,17 @@ import IPython
 
 tf.get_logger().setLevel('ERROR')
 
-
-# In[4]:
-
-
 from tapas.utils import tf_example_utils
 from tapas.protos import interaction_pb2
 from tapas.utils import number_annotation_utils
 import math
 import json
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--data_path', type=str, default='./data/test_examples_with_csv_paraphrased.json', help='Path to the input JSON data file')
-parser.add_argument('--output_path', type=str, default='./logs/tapas', help='Path to save the predictions')
-parser.add_argument('--model_path', type=str, default='./checkpoints/tapas/tapas_tabfact_inter_masklm_large_reset', help='Name of the pre-trained model to use')
-args = parser.parse_args()
-
-
-max_seq_length = 512
-vocab_file = os.path.join(args.model_path, 'vocab.txt')
-config = tf_example_utils.ClassifierConversionConfig(
-    vocab_file=vocab_file,
-    max_seq_length=max_seq_length,
-    max_column_id=max_seq_length,
-    max_row_id=max_seq_length,
-    strip_column_names=False,
-    add_aggregation_candidates=False,
-)
-converter = tf_example_utils.ToClassifierTensorflowExample(config)
-
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+  
 def convert_interactions_to_examples(tables_and_queries):
   """Calls Tapas converter to convert interaction to example."""
   for idx, (table, queries) in enumerate(tables_and_queries):
@@ -73,8 +48,7 @@ def write_tf_example(filename, examples):
       writer.write(example.SerializeToString())
 
 def predict(table_data, queries, args, verbose=False):
-  table = [list(map(lambda s: s.strip(), row.split("#"))) 
-         for row in table_data.strip().split("\n") if row.strip()]
+  table = [list(map(lambda s: s.strip(), row.split("#"))) for row in table_data.strip().split("\n") if row.strip()]
   
   examples = convert_interactions_to_examples([(table, queries)])
   write_tf_example(os.path.join(args.output_path, "tabfact/tf_examples/test.tfrecord"), examples)
@@ -119,38 +93,58 @@ def predict(table_data, queries, args, verbose=False):
   return all_results
 
 
-# # Predict
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_path', type=str, default='./data/test_examples_with_csv_paraphrased.json', help='Path to the input JSON data file')
+parser.add_argument('--output_path', type=str, default='./logs/tapas', help='Path to save the predictions')
+parser.add_argument('--model_path', type=str, default='./checkpoints/tapas/tapas_tabfact_inter_masklm_large_reset', help='Name of the pre-trained model to use')
+args = parser.parse_args()
 
 
+max_seq_length = 512
+vocab_file = os.path.join(args.model_path, 'vocab.txt')
+config = tf_example_utils.ClassifierConversionConfig(
+    vocab_file=vocab_file,
+    max_seq_length=max_seq_length,
+    max_column_id=max_seq_length,
+    max_row_id=max_seq_length,
+    strip_column_names=False,
+    add_aggregation_candidates=False,
+)
+converter = tf_example_utils.ToClassifierTensorflowExample(config)
 
-
-dataset_original = json.load(open(args.data_path, 'r'))
 args.output_path = os.path.join(args.output_path, os.path.basename(args.data_path).replace('.json', ''))
 os.makedirs(args.output_path, exist_ok=True)
 os.makedirs(os.path.join(args.output_path, 'tabfact'), exist_ok=True)
-print(args.output_path)
 os.makedirs(os.path.join(args.output_path, 'tabfact/tf_examples'), exist_ok=True)
 os.makedirs(os.path.join(args.output_path, 'tabfact/model'), exist_ok=True)
 with open(os.path.join(args.output_path, 'tabfact/model/checkpoint'), 'w') as f:
   f.write('model_checkpoint_path: "model.ckpt-0"')
 for suffix in ['.data-00000-of-00001', '.index', '.meta']:
-  # f'../../../checkpoints/tapas/tapas_tabfact_inter_masklm_large_reset/model.ckpt{suffix}', f'results/tabfact/model/model.ckpt-0{suffix}'
   shutil.copyfile(os.path.join(args.model_path, f'model.ckpt{suffix}'), 
                   os.path.join(args.output_path, f'tabfact/model/model.ckpt-0{suffix}'))
 
 all_preds_original = []
 all_labels_original = []
 
-pbar = tqdm(dataset_original.items(), total=len(dataset_original))
+dataset = load_json(args.data_path)
+pbar = tqdm(dataset.items(), total=len(dataset))
 for key, value in pbar:
     questions, labels, entity, table_csv = value
+    # print(value)
+    # questions = value["statement"]
+    # labels = value["label"]
+    # table_csv = value["table"]
+    # entity = value["table_title"]
     
     # prediction
     preds = predict(table_csv, questions, args, verbose=False)
 
     all_preds_original.extend(preds)
     all_labels_original.extend(labels)
-
+    correct = sum([p==l for p,l in zip(all_preds_original, all_labels_original)])
+    total = len(all_labels_original)
+    pbar.set_description(f"Tapas acc so far: {correct}/{total}={correct/total*100:.2f}%")
 
 correct = sum([p==l for p,l in zip(all_preds_original, all_labels_original)])
 total = len(all_labels_original)
